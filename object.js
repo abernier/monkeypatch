e4420d7dc7af4b395d810bb88590e45394448a6a
Antoine BERNIER <antoine.bernier@gmail.com>
2012-11-26 00:46:03 +0100

    all in one

--- object.js
+++
@@ -1,7 +0,0 @@
-exports.keys = keys
-
-function keys (o) {
-  var a = []
-  for (var i in o) a.push(i)
-  return a
-}
--- string.js
+++
@@ -1,5 +0,0 @@
-exports.trim = trim
-
-function trim () {
-  return this.replace(/^\s+|\s+$/g, "")
-}
